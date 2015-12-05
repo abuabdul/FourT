2c09ae4a3da760490faeec66ef4a4d9f4f80f8c2
Abubacker Siddik A <abuabdul86@hotmail.com>
2015-12-05 15:38:42 +0530

    DefaultCriteria Test added

--- src/test/java/com/abuabdul/fourt/criteria/fallback/FourTSelectAllTaskDetailCriteriaTest.java
+++ src/test/java/com/abuabdul/fourt/criteria/fallback/FourTSelectAllTaskDetailCriteriaTest.java
@@ -2,19 +2,26 @@ package com.abuabdul.fourt.criteria.fallback;
 
 import static org.mockito.Mockito.verify;
 
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.abuabdul.fourt.dao.FourTTaskDetailDAO;
 import com.abuabdul.fourt.domain.TaskDetail;
 import com.abuabdul.fourt.exception.FourTServiceException;
-import com.abuabdul.fourt.service.FourTVetoService;
+import com.abuabdul.fourt.service.FourTVetoServiceImpl;
 
 public class FourTSelectAllTaskDetailCriteriaTest {
 
 	@Mock
-	private FourTVetoService fourTVetoService;
+	private FourTTaskDetailDAO fourTTaskDetailDAO;
+
+	@Spy
+	@InjectMocks
+	private FourTVetoServiceImpl fourTVetoService;
 
 	@BeforeMethod
 	public void initMocks() {
@@ -23,11 +30,11 @@ public class FourTSelectAllTaskDetailCriteriaTest {
 
 	@Test(groups = "integration")
 	public void testDefaultSelectAllCriteria() throws FourTServiceException {
-		FourTDefaultCriteria<TaskDetail> fourTDefaultCriteria = new FourTSelectAllTaskDetailCriteria(
-				fourTVetoService);
+		FourTDefaultCriteria<TaskDetail> fourTDefaultCriteria = new FourTSelectAllTaskDetailCriteria(fourTVetoService);
 		fourTDefaultCriteria.defaultSelectAllCriteria();
 
 		verify(fourTVetoService).findAllTaskDetails();
+		verify(fourTTaskDetailDAO).findAllTaskDetails();
 	}
 
 }
